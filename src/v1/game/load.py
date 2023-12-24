65388101698db5d3003c25f00fc7d472cb768e4e
Francisco Casado <hola@franciscocasado.cl>
2023-12-24 15:44:10 -0300

    refactor: rename objects.py to physical_objects.py

--- src/v1/game/load.py
+++ src/v1/game/load.py
@@ -1,7 +1,8 @@
 import math
 import pyglet
 import random
-from game import resources, objects
+from v1.game import resources
+from v1.game.physical_object import PhysicalObject
 
 
 def distance(point_1=(0, 0), point_2=(0, 0)):
@@ -16,7 +17,7 @@ def asteroids(num_asteroids, player_position, batch=None):
         while distance((asteroid_x, asteroid_y), player_position) < 100:
             asteroid_x = random.randint(0, 800)
             asteroid_y = random.randint(0, 600)
-        new_asteroid = objects.PhysicalObject(
+        new_asteroid = PhysicalObject(
             img=resources.asteroid_image, x=asteroid_x, y=asteroid_y, batch=batch
         )
         new_asteroid.velocity_x = (random.random() - 0.5) * 40

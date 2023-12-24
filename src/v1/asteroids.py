dcf2c309ae5d7b82550c984af72db33ae05c1883
Francisco Casado <hola@franciscocasado.cl>
2023-12-24 15:24:28 -0300

    feat: make everything part of a batch

--- src/v1/asteroids.py
+++ src/v1/asteroids.py
@@ -3,25 +3,29 @@ from game import resources, load
 
 game_window = pyglet.window.Window(800, 600)
 
+main_batch = pyglet.graphics.Batch()
 
+level_label = pyglet.text.Label(
+    text="My Amazing Game",
+    x=game_window.width // 2,
+    y=game_window.height // 2,
+    anchor_x="center",
+    batch=main_batch,
+)
 
-score_label = pyglet.text.Label(text="Score: 0", x=10, y=460)
-level_label = pyglet.text.Label(text="My Amazing Game",
-                            x=game_window.width//2, y=game_window.height//2, anchor_x='center')
+player_ship = pyglet.sprite.Sprite(
+    img=resources.player_image, x=400, y=300, batch=main_batch
+)
 
+asteroids = load.asteroids(3, player_ship.position, batch=main_batch)
+score_label = pyglet.text.Label(text="Score: 0", x=10, y=575, batch=main_batch)
 
-player_ship = pyglet.sprite.Sprite(img=resources.player_image, x=400, y=300)
-asteroids = load.asteroids(3, player_ship.position)
 
 @game_window.event
 def on_draw():
     game_window.clear()
+    main_batch.draw()
 
-    level_label.draw()
-    score_label.draw()
-    player_ship.draw()
-    for asteroid in asteroids:
-        asteroid.draw()
 
 if __name__ == "__main__":
     pyglet.app.run()
--- src/v1/game/load.py
+++ src/v1/game/load.py
@@ -9,7 +9,7 @@ def distance(point_1=(0, 0), point_2=(0, 0)):
     return math.sqrt((point_1[0] - point_2[0]) ** 2 + (point_1[1] - point_2[1]) ** 2)
 
 
-def asteroids(num_asteroids, player_position):
+def asteroids(num_asteroids, player_position, batch=None):
     asteroids = []
     for i in range(num_asteroids):
         asteroid_x, asteroid_y, _ = player_position
@@ -17,7 +17,7 @@ def asteroids(num_asteroids, player_position):
             asteroid_x = random.randint(0, 800)
             asteroid_y = random.randint(0, 600)
         new_asteroid = pyglet.sprite.Sprite(
-            img=resources.asteroid_image, x=asteroid_x, y=asteroid_y
+            img=resources.asteroid_image, x=asteroid_x, y=asteroid_y, batch=batch
         )
         new_asteroid.rotation = random.randint(0, 360)
         asteroids.append(new_asteroid)
